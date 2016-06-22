b0e876a31cea5022bbaaf1c247b128f4c9981718
katowulf <katowulf@gmail.com>
2016-06-22 10:46:51 -0700

    import some boilerplate into starter

--- app/hobby-detail.component.ts
+++ app/hobby-detail.component.ts
@@ -1,9 +1,12 @@
 
 import {Component} from "@angular/core";
+import { ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
 @Component({
+  directives: [ROUTER_DIRECTIVES],
   template: `
   <h1>Detail</h1>
+  <p><button [routerLink]="['List']">Go Back</button></p>
   `
 })
 export class HobbyDetailComponent {
---
+++ app/mock-hobbies.ts
@@ -0,0 +1,73 @@
+import {Hobby} from "./hobby";
+export var HOBBIES:Hobby[] = [
+  {
+    "id": "books",
+    "name": "Books",
+    "description": "Minus a couple I read as a young kid.",
+    "count": 195,
+    "units": "books read"
+  },
+  {
+    "id": "words",
+    "name": "Words",
+    "description": "Prose, shorts, essays, and novels included.",
+    "count": 627136,
+    "units": "words written",
+  },
+  {
+    "id": "novels",
+    "name": "Novels",
+    "description": "I've started 2x more than I've finished.",
+    "count": 5,
+    "units": "novels completed"
+  },
+  {
+    "id": "soccer",
+    "name": "♡ Soccer",
+    "description": "I coach and play soccer.",
+    "count": 7,
+    "units": "years"
+  },
+  {
+    "id": "gardening",
+    "name": "Gardening",
+    "description": "I have a bit of a green thumb.",
+    "count": 0,
+    "units": null
+  },
+  {
+    "id": "magic",
+    "name": "Magic Tricks",
+    "description": "I do magic shows for kids.",
+    "count": 23,
+    "units": "tricks"
+  },
+  {
+    "id": "games",
+    "name": "Table Top Games",
+    "description": "Games I own, excluding puzzles and card games.",
+    "count": 52,
+    "units": "games owned"
+  },
+  {
+    "id": "mma",
+    "name": "Martial Arts",
+    "description": "I've studied since I was young.",
+    "count": 32,
+    "units": "years"
+  },
+  {
+    "id": "art",
+    "name": "Artistic Things",
+    "description": "I dabble in painting, art, music, and calligraphy.",
+    "count": 0,
+    "units": null
+  },
+  {
+    "id": "twist",
+    "name": "Ballon Twist Shapes",
+    "description": "Please, no more Frozen wands.",
+    "count": 11,
+    "units": "shapes I can twist"
+  }
+];
